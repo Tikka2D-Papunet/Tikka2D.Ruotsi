e1b64b34bf4a8a7676f38f4b8f24a94b09149172
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2024-07-10 12:00:12 +0300

    Ending texts are now swedish

--- Assets/Scripts/UI/EndingScript.cs
+++ Assets/Scripts/UI/EndingScript.cs
@@ -16,17 +16,13 @@ public class EndingScript : MonoBehaviour
     #endregion
     public MouseAndDartManager MaDmanager;
     public int publicScore;
-    //int howManyDartsThrown;
     public GameObject[] endings;
-    //int index;
     public TextMeshProUGUI scoreText1;
     public TextMeshProUGUI scoreText2;
     public TextMeshProUGUI scoreText3;
     public TextMeshProUGUI scoreText4;
     public Button playAgain;
     public Button backToMenu;
-    //float counter;
-    //float maxTime = 2;
     [SerializeField] InputManager inputManager;
     [SerializeField] ScoreText scoreText;
     public void IfEndingConditionsAreMet(int throwCount, int score)
@@ -49,22 +45,22 @@ public class EndingScript : MonoBehaviour
         if (newScore < 25)
         {
             endings[0].SetActive(isActiveAndEnabled);
-            scoreText1.text = "alle " + newScore + " pistettä: Hyvä alku!\nPienellä harjoittelulla kehityt varmasti vielä.";
+            scoreText1.text = "Du fick " + newScore + " pöäng. Bra början! Med lite övning blir du bättre.";
         }
         else if (newScore >= 25 && newScore < 35)
         {
             endings[1].SetActive(isActiveAndEnabled);
-            scoreText2.text = "tasan tai yli " + newScore + " pistettä: Olet selkeästi harjoitellut heittämistä!";
+            scoreText2.text = "Du fick " + newScore + " pöäng. Du har helt klart tränat på att kasta.";
         }
         else if (newScore > 34 && newScore < 45)
         {
             endings[2].SetActive(isActiveAndEnabled);
-            scoreText3.text = "tasan tai yli " + newScore + " pistettä: Taidat olla jo melko hyvä heittäjä?";
+            scoreText3.text = "Du fick " + newScore + " pöäng. Du verkar vara ganska bra på att kasta!";
         }
         else if (newScore > 44)
         {
             endings[3].SetActive(isActiveAndEnabled);
-            scoreText4.text = "tasan tai yli " + newScore + " pistettä: Sinähän olet varsinainen tikkamestari!";
+            scoreText4.text = "Du fick " + newScore + " pöäng. Du är verkligen en mästare på pilkastning!";
         }
         yield return new WaitForSeconds(0.5f);
         inputManager.GetComponent<InputManager>().isEndingMenuOpen = true;
