66ed16348d2baf6d26f0e74a4cee848bcdcd0610
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2024-07-10 11:09:52 +0300

    Revert "Copy pasting Tikka2D again..."
    This reverts commit a7dc5fb766550452c1a5c97b9514bf3f8522f08f.

--- Assets/Scripts/DestroyDarts.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class DestroyDarts : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Dart")
-            Destroy(collision.gameObject);
-    }
-}
--- Assets/Scripts/RotatingStar.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public class RotatingStar : MonoBehaviour
-{
-    float rotateSpeed = 40;
-    private void Update()
-    {
-        transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
-    }
-}
